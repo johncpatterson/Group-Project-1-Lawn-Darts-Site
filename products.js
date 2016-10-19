f673d5629d47622667e4c99595139e32fcce3eae
[John Patterson] <pattersonjc1@gmail.com>
2016-10-19 16:47:40 -0400

    began building product page

---
+++ products.js
@@ -0,0 +1,34 @@
+jQuery(function() {
+
+
+    getProducts();
+
+
+
+    function getProducts() {
+        $.ajax({
+            url: `https://json-data.herokuapp.com/darts/info`,
+            method: 'GET',
+            success: function successHandler(Products) {
+                console.log(Products);
+                    $("#putProductsHere").append(`
+                        <div class="row">
+                               <div class="col-md-1 product-info">${Products.data.product.title}</div>
+                               <div class="col-md-4"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
+                               <div class="col-md-7 product-info">${Products.data.product.description}</div>
+                        </div>
+                        `);
+            },
+        });
+    }
+
+
+
+
+
+
+
+
+
+    // end of jQuery function tag:
+});
