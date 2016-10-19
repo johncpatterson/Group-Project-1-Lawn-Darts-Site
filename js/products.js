9ef6ab44ab6c8d8e23520c49a9413a9a41982301
[John Patterson] <pattersonjc1@gmail.com>
2016-10-19 17:03:28 -0400

    built out first version of product page

--- js/products.js
+++ js/products.js
@@ -12,10 +12,22 @@ jQuery(function() {
             success: function successHandler(Products) {
                 console.log(Products);
                     $("#putProductsHere").append(`
-                        <div class="row">
-                               <div class="col-md-1 product-info">${Products.data.product.title}</div>
-                               <div class="col-md-4"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
-                               <div class="col-md-7 product-info">${Products.data.product.description}</div>
+                       <div class="row">
+                            <div class="col-md-4">
+                                   <div class="product-info">${Products.data.product.title}</div>
+                                   <div class="product-image"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
+                                   <div class="product-info">${Products.data.product.description}</div>
+                            </div>
+                            <div class="col-md-4">
+                                   <div class="product-info">${Products.data.product.title}</div>
+                                   <div class="product-image"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
+                                   <div class="product-info">${Products.data.product.description}</div>
+                            </div>
+                            <div class="col-md-4">
+                                   <div class="product-info">${Products.data.product.title}</div>
+                                   <div class="product-image"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
+                                   <div class="product-info">${Products.data.product.description}</div>
+                            </div>
                         </div>
                         `);
             },
