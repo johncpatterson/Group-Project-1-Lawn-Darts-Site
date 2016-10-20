a879e3f79a156110572c924499d7d2546cfcc953
[John Patterson] <pattersonjc1@gmail.com>
2016-10-19 21:16:16 -0400

    style tweaks on the product page

--- js/products.js
+++ js/products.js
@@ -15,18 +15,18 @@ jQuery(function() {
                        <div class="row">
                             <div class="col-md-4">
                                    <div class="product-info">${Products.data.product.title}</div>
-                                   <div class="product-image"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
-                                   <div class="product-info">${Products.data.product.description}</div>
+                                   <div class=""><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg" class="product-image"></div>
+                                   <div class="product-description">${Products.data.product.description}</div>
                             </div>
                             <div class="col-md-4">
                                    <div class="product-info">${Products.data.product.title}</div>
-                                   <div class="product-image"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
-                                   <div class="product-info">${Products.data.product.description}</div>
+                                   <div class=""><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg" class="product-image"></div>
+                                   <div class="product-description">${Products.data.product.description}</div>
                             </div>
                             <div class="col-md-4">
                                    <div class="product-info">${Products.data.product.title}</div>
-                                   <div class="product-image"><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg.jpg"></div>
-                                   <div class="product-info">${Products.data.product.description}</div>
+                                   <div class=""><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg" class="product-image"></div>
+                                   <div class="product-description">${Products.data.product.description}</div>
                             </div>
                         </div>
                         `);
