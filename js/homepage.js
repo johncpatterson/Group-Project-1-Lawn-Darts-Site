32d47811a47c4c925b5858f4c0beaf2c57a30c9e
Michael Savoia <MichaelSavoia@Michaels-MacBook-Pro.local>
2016-10-20 09:28:02 -0400

    pull check

--- js/homepage.js
+++ js/homepage.js
@@ -1,57 +1,81 @@
 $.ajax({
-	url: 'https://json-data.herokuapp.com/darts/testimonials',
-	success: function(result){
+    url: 'https://json-data.herokuapp.com/darts/testimonials',
+    success: function(result) {
 
-		var personArray = result.results
+        var personArray = result.results
 
-		$('#personName1').append(`
+        $('#personName1').append(`
 			${personArray[0].name}
 		`)
 
-		$('#quote1').append(`
+        $('#quote1').append(`
 			"${personArray[0].review}"
 		`)
 
-		$('#personName2').append(`
+        $('#personName2').append(`
 			${personArray[1].name}
 		`)
 
-		$('#quote2').append(`
+        $('#quote2').append(`
 			"${personArray[1].review}"
 		`)
 
-		$('#personName3').append(`
+        $('#personName3').append(`
 			${personArray[2].name}
 		`)
 
-		$('#quote3').append(`
+        $('#quote3').append(`
 			"${personArray[2].review}"
 		`)
-	}
+    }
 })
 
 $.ajax({
-	url: 'https://json-data.herokuapp.com/darts/companies',
-	success: function(result){
-		console.log(result);
+    url: 'https://json-data.herokuapp.com/darts/companies',
+    success: function(result) {
+        console.log(result);
 
-		var companyArray = result.results
-		console.log(companyArray[0].image_url)
+        var companyArray = result.results
+        console.log(companyArray[0].image_url)
 
-		$('.company1').append(`
+        $('.company1').append(`
 			<img src=${companyArray[0].image_url}>
 		`)
 
-		$('.company2').append(`
+        $('.company2').append(`
 			<img src=${companyArray[1].image_url}>
 		`)
 
-		$('.company3').append(`
+        $('.company3').append(`
 			<img src=${companyArray[2].image_url}>
 		`)
 
-		$('.company4').append(`
+        $('.company4').append(`
 			<img src=${companyArray[3].image_url}>
 		`)
-	}
+    }
 })
+
+
+// Modal popup
+var modal = document.getElementById("cartModal");
+var btn = document.getElementById("cartButton");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
---
+++ js/products.js
@@ -0,0 +1,63 @@
+jQuery(function() {
+
+
+    getProducts();
+
+
+
+    function getProducts() {
+        $.ajax({
+            url: `https://json-data.herokuapp.com/darts/info`,
+            method: 'GET',
+            success: function successHandler(Products) {
+                console.log(Products);
+                $("#putProductsHere").append(`
+                       <div class="row">
+                            <div class="col-md-4">
+                                   <div class="product-info">${Products.data.product.title}</div>
+                                   <div class=""><img src="https://i.kinja-img.com/gawker-media/image/upload/s---ejNL-Rr--/c_fit,fl_progressive,q_80,w_636/18ml0r9bbvce9jpg" class="product-image"></div>
+                                   <div class="product-description">${Products.data.product.description}</div>
+                            </div>
+                            <div class="col-md-4">
+                                   <div class="product-info">${Products.data.product.title}</div>
+                                   <div class=""><img src="http://photos1.blogger.com/x/blogger/4731/3161/1600/901114/jarts.jpg" class="product-image"></div>
+                                   <div class="product-description">${Products.data.product.description}</div>
+                            </div>
+                            <div class="col-md-4">
+                                   <div class="product-info">${Products.data.product.title}</div>
+                                   <div class=""><img src="http://i1.sell.com/u/GC/6P/25173365-l.jpg" class="product-image"></div>
+                                   <div class="product-description">${Products.data.product.description}</div>
+                            </div>
+                        </div>
+                        `);
+            },
+        });
+    }
+
+
+
+// // Modal popup jQuery
+// // var $modal = $("#cartModal");
+// // var $btn = $("#cartButton");
+// // var $span = $(".close");
+
+// $("#cartModal").on("click", function() {
+//     this.css("display", "block");
+// });
+
+// $("span").on("click", function() {
+//     this.css("display", "none");
+// });
+
+// $(window).on("click", function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// });
+
+
+
+
+
+    // end of jQuery function tag:
+});
